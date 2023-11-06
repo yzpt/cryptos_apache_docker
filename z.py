d47a9c5286a23ad2584aaae7e0bba0c93687511e
Yohann <yzpt.de@gmail.com>
2023-11-06 03:19:08 +0100

    pyspark insert to postgresql ok

--- z.py
+++ z.py
@@ -1,5 +1,6 @@
 from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StructField, StringType, BinaryType, IntegerType, LongType, TimestampType
+import uuid
 
 # Create Spark session
 def create_spark_connection():
@@ -8,7 +9,8 @@ def create_spark_connection():
     try:
         s_conn = SparkSession.builder \
             .appName('SparkDataStreaming') \
-            .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0,org.postgresql:postgresql:42.6.0') \
+            .config('spark.jars.packages', 'org.apache.spark:spark-sql-kafka-0-10_2.12:3.5.0') \
+            .config('spark.jars', './postgresql-42.6.0.jar') \
             .getOrCreate()
 
         print("Spark connection created successfully!")
@@ -34,46 +36,40 @@ kafka_schema = StructType([
 def connect_to_kafka(spark_conn):
     spark_df = None
     try:
-        spark_df = spark_conn.readStream \
+        # spark_df = spark_conn.readStream \
+        spark_df = spark_conn.read \
             .format('kafka') \
             .option('kafka.bootstrap.servers', 'localhost:9092') \
             .option('subscribe', 'new_topic') \
             .option('startingOffsets', 'earliest') \
             .load()
         print("Kafka DataFrame created successfully")
+        
+        print(spark_df)
     except Exception as e:
         print(f"Kafka DataFrame could not be created because: {e}")
 
     return spark_df
 
-# Define the function to write each batch to PostgreSQL
 
-def write_to_postgres(spark_df, batch_id):
-    try:
-        spark_df.write \
+
+# Connect to Kafka and write to PostgreSQL using streaming
+if __name__ == "__main__":
+    spark_df = connect_to_kafka(spark_conn)
+
+    selected_df = spark_df.selectExpr("value", "timestamp")
+    
+    # Rename columns
+    selected_df = selected_df \
+                    .withColumnRenamed("value", "message") \
+                    .withColumnRenamed("timestamp", "created_at")
+
+    selected_df.write \
             .format("jdbc") \
+            .option("driver", "org.postgresql.Driver") \
             .option("url", "jdbc:postgresql://localhost:5432/spark") \
             .option("dbtable", "spark_table") \
             .option("user", "spark") \
             .option("password", "spark") \
             .mode("append") \
             .save()
-    except Exception as e:
-        print(f"Couldn't write to PostgreSQL due to exception: {e}")
-
-# Connect to Kafka and write to PostgreSQL using streaming
-if __name__ == "__main__":
-    spark_df = connect_to_kafka(spark_conn)
-
-    # Deserialize the 'value' column from Kafka as a string and 'timestamp' as a timestamp
-    deserialized_df = spark_df.selectExpr("CAST(value AS STRING)", "CAST(timestamp AS TIMESTAMP)")
-
-    # Define the streaming query
-    query = deserialized_df \
-        .writeStream \
-        .outputMode('append') \
-        .foreachBatch(write_to_postgres) \
-        .start()
-
-    # Wait for the termination of the query
-    query.awaitTermination()
